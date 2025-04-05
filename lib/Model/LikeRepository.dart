8d8ed1886890d7080356c7f098fbab285494d327
pierre-gangneux <gangneuxpierre463@gmail.com>
2025-04-05 06:15:53 +0200

    like fonctionne

--- lib/Model/LikeRepository.dart
+++ lib/Model/LikeRepository.dart
@@ -14,18 +14,21 @@ class LikeRepository{
 
 
 
-  Future<List<Restaurant?>> getLike(Database db, User user) async {
+  Future<List<Restaurant?>> getLike(Database db, String username) async {
     // Exécuter la requête pour récupérer les données de la table RESTAURANT_FAVORIS pour l'utilisateur
     List<Map<String, dynamic>> result = await db.rawQuery(
-        'SELECT osmid FROM RESTAURANT_FAVORIS WHERE username=${user.username};'
+      'SELECT osmid FROM RESTAURANT_FAVORIS WHERE username=?;',
+      [username], // Paramètre pour remplacer le ?
     );
 
     // Convertir les résultats en instances de Restaurant
     List<Restaurant?> restaurants = result.map((row) {
       return lesRestaurant.getRestaurantById(row['osmid']);
     }).toList();
+
     return restaurants;
   }
+
 
   Future<void> addLike(Like like) async {
     await db.insert(
@@ -33,10 +36,21 @@ class LikeRepository{
         like.toMap(),
         conflictAlgorithm: ConflictAlgorithm.replace,
     );
-    like = db.query('RESTAURANT_FAVORIS', "select * from RESTAURANT_FAVORIS;");
 
-    print(like);
+    print(await this.getLike(db, like.username));
   }
+
+  Future<void> removeLike(Like like) async {
+    await db.delete(
+      'RESTAURANT_FAVORIS',
+      where: 'username = ? AND osmid = ?',
+      whereArgs: [like.username, like.osmid],
+    );
+
+    // Affichage pour vérifier le résultat
+    print(await this.getLike(db, like.username));
+  }
+
 
 
 
