7f68aa3ef5cf15029bfbd0266087cd68d26c2b9b
baptiste-mignan <baptiste.mignan@gmail.com>
2025-04-06 21:58:45 +0200

    Fix pb carte

--- lib/Views/mapView.dart
+++ lib/Views/mapView.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
-import 'package:geolocator/geolocator.dart'; // Import geolocator package
-import '../Model/Restaurant/Restaurant.dart';
+import 'package:provider/provider.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:sae_mobile/ViewModels/restaurantViewModel.dart';
+import 'package:flutter_map_cancellable_tile_provider/flutter_map_cancellable_tile_provider.dart';
+import 'package:sae_mobile/Views/restaurantDetailView.dart';
 
 class MapView extends StatefulWidget {
   const MapView({super.key});
@@ -13,38 +16,10 @@ class MapView extends StatefulWidget {
 }
 
 class MapViewState extends State<MapView> {
-  // Sample list of restaurants
-  final List<Restaurant> restaurants = [
-    Restaurant(
-      osmid: "1",
-      nomRestaurant: "Restaurant A",
-      type: "Italian",
-      etoiles: 4,
-      latitude: "48.8566",
-      longitude: "2.3522",
-    ),
-    Restaurant(
-      osmid: "2",
-      nomRestaurant: "Restaurant B",
-      type: "French",
-      etoiles: 5,
-      latitude: "48.8566",
-      longitude: "2.3622",
-    ),
-    Restaurant(
-      osmid: "3",
-      nomRestaurant: "Restaurant C",
-      type: "Japanese",
-      etoiles: 3,
-      latitude: "48.8566",
-      longitude: "2.3722",
-    ),
-  ];
-
-  // State variables
   String? selectedOsmid;
   LatLng? userLocation; // User's geolocation
   final MapController _mapController = MapController(); // Map controller
+  LatLngBounds? mapBounds; // Map bounds
 
   @override
   void initState() {
@@ -87,118 +62,166 @@ class MapViewState extends State<MapView> {
 
   @override
   Widget build(BuildContext context) {
+    final restaurantViewModel = Provider.of<RestaurantViewModel>(context);
+
+    // Get the restaurants within the current map bounds
+    final restaurantsInBounds = mapBounds != null
+        ? restaurantViewModel.getRestaurantsInBounds(
+      minLatitude: mapBounds!.south,
+      maxLatitude: mapBounds!.north,
+      minLongitude: mapBounds!.west,
+      maxLongitude: mapBounds!.east,
+    )
+        : [];
+
     return Scaffold(
       appBar: AppBar(title: const Text("Map with Restaurants")),
-      body: Stack(
+      body: Row(
         children: [
-          // FlutterMap widget
-          FlutterMap(
-            mapController: _mapController,
-            options: MapOptions(
-              initialCenter: userLocation ?? LatLng(48.8566, 2.3522), // Center of the map
-              initialZoom: 13.0,
-            ),
-            children: [
-              TileLayer(
-                urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-                subdomains: ['a', 'b', 'c'],
-              ),
-              MarkerLayer(
-                markers: [
-                  // User location marker
-                  if (userLocation != null)
-                    Marker(
-                      point: userLocation!,
-                      child: const Icon(
-                        Icons.location_pin,
-                        color: Colors.blue,
-                        size: 40.0,
-                      ),
-                    ),
-                  // Restaurant markers
-                  ...restaurants.map((restaurant) {
-                    return Marker(
-                    point: LatLng(
-                      double.parse(restaurant.latitude ?? "0"),
-                      double.parse(restaurant.longitude ?? "0"),
-                    ),
-                    child: Tooltip(
-                      message: restaurant.nomRestaurant,
-                      child: IconButton(
-                        iconSize: restaurant.osmid == selectedOsmid ? 40.0 : 30.0,
-                        icon: Icon(
-                          Icons.location_pin,
-                          color: restaurant.osmid == selectedOsmid
-                              ? Colors.red
-                              : Colors.grey,
+          // Left ListView
+          Container(
+            width: MediaQuery.of(context).size.width * 0.1, // 10% of the screen width
+            color: Colors.white,
+            child: ListView.builder(
+              itemCount: restaurantsInBounds.length,
+              itemBuilder: (context, index) {
+                final restaurant = restaurantsInBounds[index];
+                final isSelected = restaurant.osmid == selectedOsmid;
+
+                return Card(
+                  elevation: isSelected ? 4 : 1,
+                  child: Column(
+                    children: [
+                      ListTile(
+                        title: Text(restaurant.nomRestaurant),
+                        subtitle: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(restaurant.type),
+                            const SizedBox(height: 4),
+                            RatingBarIndicator(
+                              rating: restaurant.etoiles.toDouble(),
+                              itemBuilder: (context, index) => const Icon(
+                                Icons.star,
+                                color: Colors.amber,
+                              ),
+                              itemCount: 5,
+                              itemSize: 20.0,
+                              direction: Axis.horizontal,
+                            ),
+                          ],
                         ),
-                        onPressed: () {
-                          // Update the selected restaurant
+                        onTap: () {
+                          // Update the selected restaurant when tapped in the list
                           setState(() {
                             selectedOsmid = restaurant.osmid;
                           });
                           debugPrint("Selected ${restaurant.nomRestaurant}");
                         },
                       ),
-                    ),
-                  );
-                })
-                ],
-              ),
-            ],
-          ),
-          // ListView overlay in the top-left corner
-          Align(
-            alignment: Alignment.topLeft,
-            child: Container(
-              width: 200, // Adjust width as needed
-              height: 300, // Adjust height as needed
-              color: Colors.white.withValues(alpha: 0.8),
-              child: ListView.builder(
-                itemCount: restaurants.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(restaurants[index].nomRestaurant),
-                    subtitle: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(restaurants[index].type),
-                        const SizedBox(height: 4),
-                        RatingBarIndicator(
-                          rating: restaurants[index].etoiles.toDouble(),
-                          itemBuilder: (context, index) => const Icon(
-                            Icons.star,
-                            color: Colors.amber,
+                      if (isSelected)
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: ElevatedButton(
+                            onPressed: () {
+                              // Navigate to the restaurant's page
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => RestaurantDetailView(
+                                    restaurant: restaurant,
+                                  ),
+                                ),
+                              );
+                            },
+                            child: const Text("Go to Page"),
                           ),
-                          itemCount: 5,
-                          itemSize: 20.0,
-                          direction: Axis.horizontal,
                         ),
-                      ],
-                    ),
-                    onTap: () {
-                      // Update the selected restaurant when tapped in the list
-                      setState(() {
-                        selectedOsmid = restaurants[index].osmid;
-                      });
-                      debugPrint("Selected ${restaurants[index].nomRestaurant}");
-                    },
-                  );
-                },
-              ),
+                    ],
+                  ),
+                );
+              },
             ),
           ),
-          // Reset position button
-          Positioned(
-            bottom: 16,
-            right: 16,
-            child: FloatingActionButton(
-              onPressed: () {
-                if (userLocation != null) {
-                  _mapController.move(userLocation!, 13.0);
-                }
-              },
-              child: const Icon(Icons.my_location),
+          // Right Map
+          Expanded(
+            child: Stack(
+              children: [
+                FlutterMap(
+                  mapController: _mapController,
+                  options: MapOptions(
+                    initialCenter: userLocation ?? LatLng(48.8566, 2.3522),
+                    initialZoom: 13.0,
+                    onPositionChanged: (position, hasGesture) {
+                      setState(() {
+                        mapBounds = position.visibleBounds;
+                      });
+                    },
+                  ),
+                  children: [
+                    TileLayer(
+                      urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                      subdomains: ['a', 'b', 'c'],
+                      tileProvider: CancellableNetworkTileProvider(),
+                    ),
+                    MarkerLayer(
+                      markers: [
+                        // User location marker
+                        if (userLocation != null)
+                          Marker(
+                            point: userLocation!,
+                            child: const Icon(
+                              Icons.location_pin,
+                              color: Colors.blue,
+                              size: 40.0,
+                            ),
+                          ),
+                        // Restaurant markers
+                        ...restaurantsInBounds.map((restaurant) {
+                          return Marker(
+                            point: LatLng(
+                              double.parse(restaurant.latitude ?? "0"),
+                              double.parse(restaurant.longitude ?? "0"),
+                            ),
+                            child: Tooltip(
+                              message: restaurant.nomRestaurant,
+                              child: IconButton(
+                                iconSize: restaurant.osmid == selectedOsmid ? 40.0 : 30.0,
+                                icon: Icon(
+                                  Icons.location_pin,
+                                  color: restaurant.osmid == selectedOsmid
+                                      ? Colors.red
+                                      : Colors.grey,
+                                ),
+                                onPressed: () {
+                                  // Update the selected restaurant
+                                  setState(() {
+                                    selectedOsmid = restaurant.osmid;
+                                  });
+                                  debugPrint("Selected ${restaurant.nomRestaurant}");
+                                },
+                              ),
+                            ),
+                          );
+                        })
+                      ],
+                    ),
+                  ],
+                ),
+                // Reset position button
+                Positioned(
+                  bottom: 16,
+                  right: 16,
+                  child: FloatingActionButton(
+                    onPressed: () {
+                      if (userLocation != null) {
+                        _mapController.move(userLocation!, 13.0);
+                      }
+                    },
+                    child: const Icon(Icons.my_location),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
