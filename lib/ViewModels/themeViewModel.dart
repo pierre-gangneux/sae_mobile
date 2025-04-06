fb445b50e3f3c6b598637b779ca2ce536f2adda9
baptiste-mignan <baptiste.mignan@gmail.com>
2025-04-06 22:07:10 +0200

    theme enregistré dans shared preferences

--- lib/ViewModels/themeViewModel.dart
+++ lib/ViewModels/themeViewModel.dart
@@ -1,21 +1,35 @@
 import 'package:flutter/material.dart';
+import '../Model/themeRepository.dart';
 
 class ThemeViewModel extends ChangeNotifier {
+  final ThemeRepository _repository = ThemeRepository();
   bool _isDarkMode = false;
 
   bool get isDarkMode => _isDarkMode;
 
-  void toggleTheme() {
-    _isDarkMode = !_isDarkMode;
+  ThemeViewModel() {
+    _loadTheme();
+  }
+
+  Future<void> _loadTheme() async {
+    _isDarkMode = await _repository.loadTheme();
     notifyListeners();
   }
 
-  ThemeData darkTheme = ThemeData(
+  Future<void> toggleTheme() async {
+    _isDarkMode = !_isDarkMode;
+    await _repository.saveTheme(_isDarkMode);
+    notifyListeners();
+  }
+
+  ThemeData get currentTheme => _isDarkMode ? darkTheme : lightTheme;
+
+  final ThemeData darkTheme = ThemeData(
     brightness: Brightness.dark,
     primarySwatch: Colors.blue,
     scaffoldBackgroundColor: Colors.black,
     cardColor: Colors.grey[800],
-    cardTheme: CardTheme(color: Colors.grey[700]), // Dark mode card theme
+    cardTheme: CardTheme(color: Colors.grey[700]),
     bottomNavigationBarTheme: const BottomNavigationBarThemeData(
       backgroundColor: Colors.black,
       selectedItemColor: Colors.blue,
@@ -29,12 +43,12 @@ class ThemeViewModel extends ChangeNotifier {
     ),
   );
 
-  ThemeData lightTheme = ThemeData(
+  final ThemeData lightTheme = ThemeData(
     brightness: Brightness.light,
     primarySwatch: Colors.blue,
     scaffoldBackgroundColor: Colors.white,
     cardColor: Colors.grey[200],
-    cardTheme: CardTheme(color: Colors.grey[600]), // Light mode card theme
+    cardTheme: CardTheme(color: Colors.grey[600]),
     bottomNavigationBarTheme: const BottomNavigationBarThemeData(
       backgroundColor: Colors.white,
       selectedItemColor: Colors.blue,
@@ -47,8 +61,4 @@ class ThemeViewModel extends ChangeNotifier {
       titleTextStyle: TextStyle(color: Colors.white, fontSize: 20),
     ),
   );
-
-  ThemeData get currentTheme {
-    return _isDarkMode ? darkTheme : lightTheme;
-  }
 }
