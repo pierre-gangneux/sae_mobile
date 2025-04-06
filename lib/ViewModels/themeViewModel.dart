8b417259a668a7459547de430e18d11df8d64323
Lucas <lucas.devers-dore@etu.univ-orleans.fr>
2025-04-06 18:01:39 +0200

    rajout du fichier themeViewModel.dart

---
+++ lib/ViewModels/themeViewModel.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class ThemeViewModel extends ChangeNotifier {
+  bool _isDarkMode = false;
+
+  bool get isDarkMode => _isDarkMode;
+
+  void toggleTheme() {
+    _isDarkMode = !_isDarkMode;
+    notifyListeners();
+  }
+
+  ThemeData get currentTheme {
+    return _isDarkMode
+        ? ThemeData(
+      brightness: Brightness.dark,
+      primarySwatch: Colors.blue,
+      scaffoldBackgroundColor: Colors.black,
+      cardColor: Colors.grey[800],
+      cardTheme: CardTheme(color: Colors.grey[700]), // Dark mode card theme
+      bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+        backgroundColor: Colors.black,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        elevation: 5,
+      ),
+      appBarTheme: const AppBarTheme(
+        backgroundColor: Colors.black,
+        elevation: 0,
+        titleTextStyle: TextStyle(color: Colors.white, fontSize: 20),
+      ),
+    )
+        : ThemeData(
+      brightness: Brightness.light,
+      primarySwatch: Colors.blue,
+      scaffoldBackgroundColor: Colors.white,
+      cardColor: Colors.grey[200],
+      cardTheme: CardTheme(color: Colors.grey[600]), // Light mode card theme
+      bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+        backgroundColor: Colors.white,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        elevation: 5,
+      ),
+      appBarTheme: const AppBarTheme(
+        backgroundColor: Colors.blue,
+        elevation: 0,
+        titleTextStyle: TextStyle(color: Colors.white, fontSize: 20),
+      ),
+    );
+  }
+}
